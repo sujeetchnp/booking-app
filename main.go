647c3e0a3c98a8245bf3658aee42974ea5d411f1
sujeetchnp <sujeetchnp@gmail.com>
2022-11-04 20:14:57 -0400

    Added main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	fmt.Println(remainingTickets)
+	fmt.Println(&remainingTickets)
+
+}
